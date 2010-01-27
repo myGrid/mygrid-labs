bd90ffd05aa21414b74e4facbe8ff4d90062df9e
paolo@mygrid.org.uk <paolo@mygrid.org.uk@d2a11066-01d3-11df-8b0e-05a2803efa1e>
2010-01-27 09:56:29 +0000

    initial ci

---
+++ provenance-analysis/trunk/provenance-friendliness-analysis/src/main/java/net/sf/taverna/t2/provenance/consumer/PropertiesReader.java
@@ -0,0 +1,24 @@
+package net.sf.taverna.t2.provenance.consumer;
+
+
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+public class PropertiesReader {
+	private static final String BUNDLE_NAME = "net.sf.taverna.t2.provenance.consumer.resources.analyser"; //$NON-NLS-1$
+
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
+			.getBundle(BUNDLE_NAME);
+
+	private PropertiesReader() {
+	}
+
+	public static String getString(String key) {
+		try {
+			return RESOURCE_BUNDLE.getString(key);
+		} catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+}
