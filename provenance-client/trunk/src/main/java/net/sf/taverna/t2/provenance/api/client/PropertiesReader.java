82eceb0bad404bf96710658539c2b168f3924343
paolo@mygrid.org.uk <paolo@mygrid.org.uk@d2a11066-01d3-11df-8b0e-05a2803efa1e>
2010-06-22 17:05:37 +0000

    fixing query result rendering

--- provenance-client/trunk/src/main/java/net/sf/taverna/t2/provenance/api/client/PropertiesReader.java
+++ provenance-client/trunk/src/main/java/net/sf/taverna/t2/provenance/api/client/PropertiesReader.java
@@ -5,11 +5,16 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class PropertiesReader {
-	private static final String BUNDLE_NAME = "net.sf.taverna.t2.provenance.api.client.resources.APIClient"; //$NON-NLS-1$
+	private static String BUNDLE_NAME = "net.sf.taverna.t2.provenance.api.client.resources.APIClient"; //$NON-NLS-1$
 
-	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
-			.getBundle(BUNDLE_NAME);
+	private static ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
+	public static void setBundleName(String bundleName) { 
+		BUNDLE_NAME = bundleName;
+		RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+		}
+	
+	
 	private PropertiesReader() {
 	}
 
