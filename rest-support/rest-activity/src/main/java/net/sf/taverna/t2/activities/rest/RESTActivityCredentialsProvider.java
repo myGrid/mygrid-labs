0b039a65bf7aa042c6e7f537e803539ae8d1e57f
sergejs@mygrid.org.uk <sergejs@mygrid.org.uk@d2a11066-01d3-11df-8b0e-05a2803efa1e>
2010-06-29 11:36:31 +0000

    REST Support - REST activity credentials provider updated to fetch credentials from CredentialManager in a better way (some analysis of port numbers / protocols is done)

--- rest-support/rest-activity/src/main/java/net/sf/taverna/t2/activities/rest/RESTActivityCredentialsProvider.java
+++ rest-support/rest-activity/src/main/java/net/sf/taverna/t2/activities/rest/RESTActivityCredentialsProvider.java
@@ -1,6 +1,7 @@
 package net.sf.taverna.t2.activities.rest;
 
 import java.net.URI;
+import java.net.URLEncoder;
 import java.security.Principal;
 
 import javax.management.remote.JMXPrincipal;
@@ -11,72 +12,125 @@ import net.sf.taverna.t2.security.credentialmanager.UsernamePassword;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.Credentials;
 import org.apache.http.client.CredentialsProvider;
+import org.apache.log4j.Logger;
 import org.springframework.core.type.StandardClassMetadata;
 
 /**
+ * This CredentialsProvider acts as a mediator between the Apache HttpClient
+ * and Taverna's CredentialManager that stores all user's credentials.
+ * 
+ * The only role of it is to retrieve stored details from CredentialManager
+ * when they are required for HTTP authentication.
  * 
  * @author Sergejs Aleksejevs
  */
 public class RESTActivityCredentialsProvider implements CredentialsProvider
 {
-
-  public void clear() {
-    // TODO Auto-generated method stub
-
-  }
-
+  
   public Credentials getCredentials(AuthScope authscope)
   {
+    // TODO - remove print out
     System.out.println(authscope.getHost() + "\n" + authscope.getPort() + "\n" + authscope.getRealm() + "\n" + authscope.getScheme() + "\n\n");
+    
+    
+    String AUTHENTICATION_REQUEST_MSG = "This REST service requires authentication in " + authscope.getRealm();
     
     try {
       CredentialManager credManager = CredentialManager.getInstance();
-      
+      UsernamePassword credentials = null;
       
       /*
-       * Append REALM - always
-       * 
        * if port is 80 - use HTTP, don't append port
        * if port is 443 - use HTTPS, don't append port
        * any other port - append port + do 2 tests:
        * --- test HTTPS first  has...()
        * --- if not there, do get...() for HTTP (which will save the thing)
-       * 
+       * (save both these entries for HTTP + HTTPS if not there)
        * 
        */
-      UsernamePassword credentials = credManager.getUsernameAndPasswordForService(
-          URI.create("http://" + authscope.getHost()+ ":" + authscope.getPort() + "/#" + authscope.getRealm()), true, "This REST service requires authentication + REALM");  // TODO - HACK!!! (can't just add HTTP)
-      System.out.println(credentials.getUsername() + " - " + credentials.getPasswordAsString());
+      
+      // build the service URI back to front 
+      StringBuilder serviceURI = new StringBuilder();
+      serviceURI.insert(0, "/#" + URLEncoder.encode(authscope.getRealm(), "UTF-16"));
+      if (authscope.getPort() != 80 && authscope.getPort() != 443) {
+        // non-default port - add port name to the URI
+        serviceURI.insert(0, ":" + authscope.getPort());
+      }
+      serviceURI.insert(0, authscope.getHost());
+      serviceURI.insert(0, "://");
+      
+      
+      // now the URI is complete, apart from the protocol name
+      if (authscope.getPort() == 80 || authscope.getPort() == 443)
+      {
+        // definitely HTTP or HTTPS
+        serviceURI.insert(0, (authscope.getPort() == 80 ? "http" : "https"));
+        
+        // request credentials from CrendentialManager
+        credentials = credManager.getUsernameAndPasswordForService(
+            URI.create(serviceURI.toString()), true, AUTHENTICATION_REQUEST_MSG);
+      }
+      else {
+        // non-default port - will need to try both HTTP and HTTPS;
+        // just check (no pop-up will be shown) if credentials are there - one protocol that
+        // matched will be used; if 
+        if (credManager.hasUsernamePasswordForService(URI.create("https" + serviceURI.toString()))) {
+          credentials = credManager.getUsernameAndPasswordForService(
+              URI.create("https" + serviceURI.toString()), true, AUTHENTICATION_REQUEST_MSG);
+        }
+        else if (credManager.hasUsernamePasswordForService(URI.create("http" + serviceURI.toString()))) {
+          credentials = credManager.getUsernameAndPasswordForService(
+              URI.create("http" + serviceURI.toString()), true, AUTHENTICATION_REQUEST_MSG);
+        }
+        else {
+          // non of the two options succeeded, request details with a popup for HTTP, then save second entry with HTTPS
+          credentials = credManager.getUsernameAndPasswordForService(
+              URI.create("http" + serviceURI.toString()), true, AUTHENTICATION_REQUEST_MSG);
+          
+          if (credentials != null) {
+            credManager.saveUsernameAndPasswordForService(credentials, URI.create("https" + serviceURI.toString()));
+          }
+        }
+      }
+      
       
       if (credentials != null) {
+        System.out.println("credentials obtained");
         return new RESTActivityCredentials(credentials.getUsername(), credentials.getPasswordAsString());
       }
     }
     catch (Exception e) {
-      System.err.println(e);
+      // TODO - enable the "proper" way of logging
+//      Logger.getLogger(RESTActivityCredentialsProvider.class).error(
+//          "Unexpected error while trying to obtain user's credential from CredentialManager", e);
+      System.err.println("Unexpected error while trying to obtain user's credential from CredentialManager\n" + e);
     }
     
     // error or nothing was found
+    System.out.println("credentials NOT found");
     return null;
   }
-
-  public void setCredentials(AuthScope authscope, Credentials credentials) {
-    // TODO Auto-generated method stub
-  }
   
+  
+  public void setCredentials(AuthScope authscope, Credentials credentials) { /* do nothing */ }
+  public void clear() { /* do nothing */ }
   
   
   /**
+   * This class encapsulates user's credentials that this CredentialsProvider can
+   * pass to Apache HttpClient.
    * 
    * @author Sergejs Aleksejevs
    */
   public class RESTActivityCredentials implements Credentials
   {
+    // this seems to be the simplest existing implementation of Principal interface
+    // TODO - however, check this it is safe to use JMXPrincipal for this purpose
     private final JMXPrincipal user;
     private final String password;
 
     public RESTActivityCredentials(String username, String password) {
-      this.user = new JMXPrincipal(username); // TODO - check this is safe to do? unknown class
+      this.user = new JMXPrincipal(username);
       this.password = password;
     }
 
