64f209f824eaa7d9f26b82563a89ec6e63e88786
Jiten Bhagat <mail@jits.co.uk>
2010-07-01 13:10:22 +0000

    Generated code for the new workflow.ecore model.

---
+++ taverna-tng/trunk/mockup3/uk.org.taverna.tng.mockup3.models/src/uk/org/taverna/tng/mockup3/models/workflow/ComponentInstanceInput.java
@@ -0,0 +1,21 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package uk.org.taverna.tng.mockup3.models.workflow;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Component Instance Input</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see uk.org.taverna.tng.mockup3.models.workflow.WorkflowPackage#getComponentInstanceInput()
+ * @model
+ * @generated
+ */
+public interface ComponentInstanceInput extends Port, Receiver {
+} // ComponentInstanceInput
---
+++ taverna-tng/trunk/mockup3/uk.org.taverna.tng.mockup3.models/src/uk/org/taverna/tng/mockup3/models/workflow/WorkflowFactory.java
@@ -0,0 +1,91 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package uk.org.taverna.tng.mockup3.models.workflow;
+
+import org.eclipse.emf.ecore.EFactory;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Factory</b> for the model.
+ * It provides a create method for each non-abstract class of the model.
+ * <!-- end-user-doc -->
+ * @see uk.org.taverna.tng.mockup3.models.workflow.WorkflowPackage
+ * @generated
+ */
+public interface WorkflowFactory extends EFactory {
+	/**
+	 * The singleton instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	WorkflowFactory eINSTANCE = uk.org.taverna.tng.mockup3.models.workflow.impl.WorkflowFactoryImpl.init();
+
+	/**
+	 * Returns a new object of class '<em>Component Instance</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Component Instance</em>'.
+	 * @generated
+	 */
+	ComponentInstance createComponentInstance();
+
+	/**
+	 * Returns a new object of class '<em>Input</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Input</em>'.
+	 * @generated
+	 */
+	WorkflowInput createWorkflowInput();
+
+	/**
+	 * Returns a new object of class '<em>Workflow</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Workflow</em>'.
+	 * @generated
+	 */
+	Workflow createWorkflow();
+
+	/**
+	 * Returns a new object of class '<em>Output</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Output</em>'.
+	 * @generated
+	 */
+	WorkflowOutput createWorkflowOutput();
+
+	/**
+	 * Returns a new object of class '<em>Component Instance Input</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Component Instance Input</em>'.
+	 * @generated
+	 */
+	ComponentInstanceInput createComponentInstanceInput();
+
+	/**
+	 * Returns a new object of class '<em>Component Instance Output</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Component Instance Output</em>'.
+	 * @generated
+	 */
+	ComponentInstanceOutput createComponentInstanceOutput();
+
+	/**
+	 * Returns the package supported by this factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the package supported by this factory.
+	 * @generated
+	 */
+	WorkflowPackage getWorkflowPackage();
+
+} //WorkflowFactory
