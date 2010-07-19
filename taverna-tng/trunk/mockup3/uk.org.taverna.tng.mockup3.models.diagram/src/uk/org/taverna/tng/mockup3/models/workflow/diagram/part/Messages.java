b044dfdef37b5e043b8bb3d01a636dc46dbd5ed5
Rishi Ramgolam <rishiramgolam@users.noreply.github.com>
2010-07-19 16:27:18 +0000

    Updated Diagram with Side Affixed nodes (nodules)

---
+++ taverna-tng/trunk/mockup3/uk.org.taverna.tng.mockup3.models.diagram/src/uk/org/taverna/tng/mockup3/models/workflow/diagram/part/Messages.java
@@ -0,0 +1,434 @@
+package uk.org.taverna.tng.mockup3.models.workflow.diagram.part;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * @generated
+ */
+public class Messages extends NLS {
+
+	/**
+	 * @generated
+	 */
+	static {
+		NLS.initializeMessages("messages", Messages.class); //$NON-NLS-1$
+	}
+
+	/**
+	 * @generated
+	 */
+	private Messages() {
+	}
+
+	/**
+	 * @generated
+	 */
+	public static String CreateShortcutAction_OpenModelTitle;
+
+	/**
+	 * @generated
+	 */
+	public static String CreateShortcutAction_WizardTitle;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowCreationWizardTitle;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowCreationWizard_DiagramModelFilePageTitle;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowCreationWizard_DiagramModelFilePageDescription;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowCreationWizard_DomainModelFilePageTitle;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowCreationWizard_DomainModelFilePageDescription;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowCreationWizardOpenEditorError;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowCreationWizardCreationError;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowCreationWizardPageExtensionError;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowDiagramEditorUtil_OpenModelResourceErrorDialogTitle;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowDiagramEditorUtil_OpenModelResourceErrorDialogMessage;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowDiagramEditorUtil_CreateDiagramProgressTask;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowDiagramEditorUtil_CreateDiagramCommandLabel;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowDocumentProvider_isModifiable;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowDocumentProvider_handleElementContentChanged;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowDocumentProvider_IncorrectInputError;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowDocumentProvider_NoDiagramInResourceError;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowDocumentProvider_DiagramLoadingError;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowDocumentProvider_UnsynchronizedFileSaveError;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowDocumentProvider_SaveDiagramTask;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowDocumentProvider_SaveNextResourceTask;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowDocumentProvider_SaveAsOperation;
+
+	/**
+	 * @generated
+	 */
+	public static String InitDiagramFile_ResourceErrorDialogTitle;
+
+	/**
+	 * @generated
+	 */
+	public static String InitDiagramFile_ResourceErrorDialogMessage;
+
+	/**
+	 * @generated
+	 */
+	public static String InitDiagramFile_WizardTitle;
+
+	/**
+	 * @generated
+	 */
+	public static String InitDiagramFile_OpenModelFileDialogTitle;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowNewDiagramFileWizard_CreationPageName;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowNewDiagramFileWizard_CreationPageTitle;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowNewDiagramFileWizard_CreationPageDescription;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowNewDiagramFileWizard_RootSelectionPageName;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowNewDiagramFileWizard_RootSelectionPageTitle;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowNewDiagramFileWizard_RootSelectionPageDescription;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowNewDiagramFileWizard_RootSelectionPageSelectionTitle;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowNewDiagramFileWizard_RootSelectionPageNoSelectionMessage;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowNewDiagramFileWizard_RootSelectionPageInvalidSelectionMessage;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowNewDiagramFileWizard_InitDiagramCommand;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowNewDiagramFileWizard_IncorrectRootError;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowDiagramEditor_SavingDeletedFile;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowDiagramEditor_SaveAsErrorTitle;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowDiagramEditor_SaveAsErrorMessage;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowDiagramEditor_SaveErrorTitle;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowDiagramEditor_SaveErrorMessage;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowElementChooserDialog_SelectModelElementTitle;
+
+	/**
+	 * @generated
+	 */
+	public static String ShortcutCreationWizard_ReferencedElementSelectionPageName;
+
+	/**
+	 * @generated
+	 */
+	public static String ShortcutCreationWizard_ReferencedElementSelectionPageTitle;
+
+	/**
+	 * @generated
+	 */
+	public static String ShortcutCreationWizard_ReferencedElementSelectionPageDescription;
+
+	/**
+	 * @generated
+	 */
+	public static String ShortcutCreationWizard_ReferencedElementSelectionPageMessage;
+
+	/**
+	 * @generated
+	 */
+	public static String ShortcutCreationWizard_ReferencedElementSelectionPageEmptyError;
+
+	/**
+	 * @generated
+	 */
+	public static String ShortcutCreationWizard_ReferencedElementSelectionPageInvalidError;
+
+	/**
+	 * @generated
+	 */
+	public static String ModelElementSelectionPageMessage;
+
+	/**
+	 * @generated
+	 */
+	public static String ValidateActionMessage;
+
+	/**
+	 * @generated
+	 */
+	public static String Workflow1Group_title;
+
+	/**
+	 * @generated
+	 */
+	public static String ComponentInstance1CreationTool_title;
+
+	/**
+	 * @generated
+	 */
+	public static String ComponentInstance1CreationTool_desc;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowInput2CreationTool_title;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowInput2CreationTool_desc;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowOutput3CreationTool_title;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowOutput3CreationTool_desc;
+
+	/**
+	 * @generated
+	 */
+	public static String DiagramEditorActionBarAdvisor_DefaultFileEditorTitle;
+
+	/**
+	 * @generated
+	 */
+	public static String DiagramEditorActionBarAdvisor_DefaultFileEditorMessage;
+
+	/**
+	 * @generated
+	 */
+	public static String DiagramEditorActionBarAdvisor_DefaultEditorOpenErrorTitle;
+
+	/**
+	 * @generated
+	 */
+	public static String DiagramEditorActionBarAdvisor_AboutDialogTitle;
+
+	/**
+	 * @generated
+	 */
+	public static String DiagramEditorActionBarAdvisor_AboutDialogMessage;
+
+	/**
+	 * @generated
+	 */
+	public static String ApplicationMenuName_File;
+
+	/**
+	 * @generated
+	 */
+	public static String ApplicationMenuName_Edit;
+
+	/**
+	 * @generated
+	 */
+	public static String ApplicationMenuName_Window;
+
+	/**
+	 * @generated
+	 */
+	public static String ApplicationMenuName_Help;
+
+	/**
+	 * @generated
+	 */
+	public static String ApplicationMenuName_New;
+
+	/**
+	 * @generated
+	 */
+	public static String DiagramEditorWorkbenchWindowAdvisor_Title;
+
+	/**
+	 * @generated
+	 */
+	public static String WizardNewFileCreationPage_FileLabel;
+
+	/**
+	 * @generated
+	 */
+	public static String WizardNewFileCreationPage_BrowseButton;
+
+	/**
+	 * @generated
+	 */
+	public static String WizardNewFileCreationPage_SelectNewFileDialog;
+
+	/**
+	 * @generated
+	 */
+	public static String WizardNewFileCreationPage_EmptyFileNameError;
+
+	/**
+	 * @generated
+	 */
+	public static String WizardNewFileCreationPage_InvalidFileNameError;
+
+	/**
+	 * @generated
+	 */
+	public static String CommandName_OpenDiagram;
+
+	/**
+	 * @generated
+	 */
+	public static String AbstractParser_UnexpectedValueType;
+
+	/**
+	 * @generated
+	 */
+	public static String AbstractParser_WrongStringConversion;
+
+	/**
+	 * @generated
+	 */
+	public static String AbstractParser_UnknownLiteral;
+
+	/**
+	 * @generated
+	 */
+	public static String MessageFormatParser_InvalidInputError;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowModelingAssistantProviderTitle;
+
+	/**
+	 * @generated
+	 */
+	public static String WorkflowModelingAssistantProviderMessage;
+
+	//TODO: put accessor fields manually	
+}
