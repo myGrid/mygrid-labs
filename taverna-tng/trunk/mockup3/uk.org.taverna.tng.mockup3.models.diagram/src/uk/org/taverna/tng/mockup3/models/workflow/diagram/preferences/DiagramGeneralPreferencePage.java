265090851ce83b50ac22d5c7f99726528024761f
rishi@mygrid.org.uk <rishi@mygrid.org.uk@d2a11066-01d3-11df-8b0e-05a2803efa1e>
2010-07-19 16:27:18 +0000

    Updated Diagram with Side Affixed nodes (nodules)

---
+++ taverna-tng/trunk/mockup3/uk.org.taverna.tng.mockup3.models.diagram/src/uk/org/taverna/tng/mockup3/models/workflow/diagram/preferences/DiagramGeneralPreferencePage.java
@@ -0,0 +1,19 @@
+package uk.org.taverna.tng.mockup3.models.workflow.diagram.preferences;
+
+import org.eclipse.gmf.runtime.diagram.ui.preferences.DiagramsPreferencePage;
+
+import uk.org.taverna.tng.mockup3.models.workflow.diagram.part.WorkflowDiagramEditorPlugin;
+
+/**
+ * @generated
+ */
+public class DiagramGeneralPreferencePage extends DiagramsPreferencePage {
+
+	/**
+	 * @generated
+	 */
+	public DiagramGeneralPreferencePage() {
+		setPreferenceStore(WorkflowDiagramEditorPlugin.getInstance()
+				.getPreferenceStore());
+	}
+}
