bebb16a7bdd206153b4c2b83630887bd4020de47
Jiten Bhagat <mail@jits.co.uk>
2010-07-22 13:13:02 +0000

    Latest work on component definitions (using Eclipse Extension Points) (WORK IN PROGRESS).

--- taverna-tng/trunk/mockup3/uk.org.taverna.tng.mockup3.core/src/uk/org/taverna/tng/mockup3/core/ComponentDefinition.java
+++ taverna-tng/trunk/mockup3/uk.org.taverna.tng.mockup3.core/src/uk/org/taverna/tng/mockup3/core/ComponentDefinition.java
@@ -1,5 +1,61 @@
 package uk.org.taverna.tng.mockup3.core;
 
-public abstract class ComponentDefinition implements IComponentDefinition {
+import org.eclipse.core.runtime.IConfigurationElement;
+
+public class ComponentDefinition implements IComponentDefinition {
+	
+	public final static String ATTR_ID = "id";
+	public final static String ATTR_NAME = "name";
+	public final static String ATTR_GROUP = "group";
+	public final static String ATTR_DESCRIPTION = "description";
+	public final static String ATTR_ICON = "icon";
+	
+	private IConfigurationElement element;
+	
+	private String id;
+	private String name;
+	private String group;
+	private String description;
+	private String icon; 
+	
+	public ComponentDefinition(String id, String name, String group, String description, String icon) {
+		this.id = id;
+		this.name = name;
+		this.group = group;
+		this.description = description;
+		this.icon = icon;
+	}
+	
+	public ComponentDefinition(IConfigurationElement element) {
+		this.element = element;
+		this.id = element.getAttribute(ATTR_ID);
+		this.name = element.getAttribute(ATTR_NAME);
+		this.group = element.getAttribute(ATTR_GROUP);
+		this.description = element.getAttribute(ATTR_DESCRIPTION);
+		this.icon = element.getAttribute(ATTR_ICON);
+		
+		// TODO: See http://www.eclipse.org/forums/index.php?&t=msg&th=97717 for more info on how to load up an ImageDescriptor for the icon.
+		// BUT this requires JFace (for the ImageDescriptor class), so maybe this should be done elsewhere.
+	}
+
+	public String getExternalId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getGroup() {
+		return group;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+	public String getIcon() {
+		return icon;
+	}
 
 }
--- taverna-tng/trunk/mockup3/uk.org.taverna.tng.mockup3.workbench/src/uk/org/taverna/tng/mockup3/workbench/views/ComponentPaletteView.java
+++ taverna-tng/trunk/mockup3/uk.org.taverna.tng.mockup3.workbench/src/uk/org/taverna/tng/mockup3/workbench/views/ComponentPaletteView.java
@@ -1,111 +1,32 @@
 package uk.org.taverna.tng.mockup3.workbench.views;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.eclipse.jface.viewers.IStructuredContentProvider;
-import org.eclipse.jface.viewers.ITableLabelProvider;
-import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TreeViewer;
-import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
-import org.eclipse.swt.events.ControlAdapter;
-import org.eclipse.swt.events.ControlEvent;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.layout.RowData;
-import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.Sash;
-import org.eclipse.ui.ISharedImages;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.FilteredTree;
 import org.eclipse.ui.dialogs.PatternFilter;
 import org.eclipse.ui.part.ViewPart;
 
-import uk.org.taverna.tng.mockup3.workbench.biocatalogue.BioCatalogueClient;
-import uk.org.taverna.tng.mockup3.workbench.biocatalogue.BioCatalogueUtil;
-import uk.org.taverna.tng.mockup3.workbench.biocatalogue.QuerySearchResults;
-import uk.org.taverna.tng.mockup3.workbench.biocatalogue.Resource;
+import uk.org.taverna.tng.mockup3.core.ComponentDefinitionsRegistry;
 import uk.org.taverna.tng.mockup3.core.ISearchTermProvider;
+import uk.org.taverna.tng.mockup3.workbench.util.CustomContentProvider;
+import uk.org.taverna.tng.mockup3.workbench.util.CustomLabelProvider;
 
 public class ComponentPaletteView extends ViewPart implements ISearchTermProvider{
 	public static final String ID = "uk.org.taverna.tng.mockup3.workbench.views.ComponentPaletteView";
 
 	protected static final int SASH_LIMIT = 50;
 	
-	private static BioCatalogueClient biocatClient = new BioCatalogueClient();
-	
 	private Composite parentContainer;
 	private SashForm containerSashForm;
 	private Button searchMoreButton;
 	private FilteredTree currentComponentsTree;
 	private TreeViewer searchComponentsTree;
-
-	/**
-	 * The content provider class is responsible for providing objects to the
-	 * view. It can wrap existing objects in adapters or simply return objects
-	 * as-is. These objects may be sensitive to the current input of the view,
-	 * or ignore it and always show the same content (like Task List, for
-	 * example).
-	 */
-	class ViewContentProvider implements IStructuredContentProvider {
-		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
-		}
-
-		public void dispose() {
-		}
-
-		public Object[] getElements(Object parent) {
-			List<String> serviceNames = new ArrayList<String>();
-			
-			String query = "blast";
-			
-			String searchURL = BioCatalogueUtil.appendURLParameter(BioCatalogueClient.API_SEARCH_URL, "q", query);
-			
-			QuerySearchResults searchResults = new QuerySearchResults();
-			
-		    try
-		    {
-		      searchResults.addSearchResults(biocatClient.getBioCatalogueSearchData(searchURL));
-		      System.out.println("INFO: " + searchResults.getTotalItemCount(Resource.SERVICE_TYPE)  + " services found.");
-		    }
-		    catch (Exception e) {
-		      System.err.println("ERROR: Couldn't execute initial phase of a search by query, details below:");
-		      e.printStackTrace();
-		    }
-			
-			return serviceNames.toArray();	
-		}
-	}
-
-	class ViewLabelProvider extends LabelProvider implements
-			ITableLabelProvider {
-		public String getColumnText(Object obj, int index) {
-			return getText(obj);
-		}
-
-		public Image getColumnImage(Object obj, int index) {
-			return getImage(obj);
-		}
-
-		public Image getImage(Object obj) {
-			return PlatformUI.getWorkbench().getSharedImages().getImage(
-					ISharedImages.IMG_OBJ_ELEMENT);
-		}
-	}
 
 	/**
 	 * This is a callback that will allow us to create the viewer and initialize
@@ -137,6 +58,11 @@ public class ComponentPaletteView extends ViewPart implements ISearchTermProvide
 		
 		currentComponentsTree = new FilteredTree(top, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL, new PatternFilter(), true);
 		currentComponentsTree.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		TreeViewer currentComponentsTreeViewer = currentComponentsTree.getViewer();
+		getSite().setSelectionProvider(currentComponentsTreeViewer);
+		currentComponentsTreeViewer.setContentProvider(new CustomContentProvider());
+		currentComponentsTreeViewer.setLabelProvider(new CustomLabelProvider());
+		currentComponentsTreeViewer.setInput(ComponentDefinitionsRegistry.INSTANCE.getAllEntries());
 		
 		Composite bottom = new Composite(containerSashForm, SWT.NONE);
 		bottom.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
