3432685932189f1e6b8fc58aa3df34d579d5772b
Jiten Bhagat <mail@jits.co.uk>
2010-07-22 22:31:10 +0000

    Added "family" field to ComponentDefinition.

--- taverna-tng/trunk/mockup3/uk.org.taverna.tng.mockup3.core/src/uk/org/taverna/tng/mockup3/core/ComponentDefinition.java
+++ taverna-tng/trunk/mockup3/uk.org.taverna.tng.mockup3.core/src/uk/org/taverna/tng/mockup3/core/ComponentDefinition.java
@@ -6,23 +6,26 @@ public class ComponentDefinition implements IComponentDefinition {
 	
 	public final static String ATTR_ID = "id";
 	public final static String ATTR_NAME = "name";
-	public final static String ATTR_GROUP = "group";
 	public final static String ATTR_DESCRIPTION = "description";
+	public final static String ATTR_GROUP = "group";
+	public final static String ATTR_FAMILY = "family";
 	public final static String ATTR_ICON = "icon";
 	
 	private IConfigurationElement element;
 	
-	private String id;
-	private String name;
-	private String group;
-	private String description;
-	private String icon; 
+	private final String id;
+	private final String name;
+	private final String description;
+	private final String group;
+	private final String family;
+	private final String icon; 
 	
-	public ComponentDefinition(String id, String name, String group, String description, String icon) {
+	public ComponentDefinition(String id, String name, String description, String group, String family, String icon) {
 		this.id = id;
 		this.name = name;
-		this.group = group;
 		this.description = description;
+		this.group = group;
+		this.family = family;
 		this.icon = icon;
 	}
 	
@@ -30,8 +33,9 @@ public class ComponentDefinition implements IComponentDefinition {
 		this.element = element;
 		this.id = element.getAttribute(ATTR_ID);
 		this.name = element.getAttribute(ATTR_NAME);
-		this.group = element.getAttribute(ATTR_GROUP);
 		this.description = element.getAttribute(ATTR_DESCRIPTION);
+		this.group = element.getAttribute(ATTR_GROUP);
+		this.family = element.getAttribute(ATTR_FAMILY);
 		this.icon = element.getAttribute(ATTR_ICON);
 		
 		// TODO: See http://www.eclipse.org/forums/index.php?&t=msg&th=97717 for more info on how to load up an ImageDescriptor for the icon.
@@ -45,13 +49,17 @@ public class ComponentDefinition implements IComponentDefinition {
 	public String getName() {
 		return name;
 	}
+	
+	public String getDescription() {
+		return description;
+	}
 
 	public String getGroup() {
 		return group;
 	}
 
-	public String getDescription() {
-		return description;
+	public String getFamily() {
+		return family;
 	}
 	
 	public String getIcon() {
