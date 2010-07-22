9c5f18ac735c8412343e88628c41d00aa7496c26
Jiten Bhagat <mail@jits.co.uk>
2010-07-22 15:20:07 +0000

    Added icons and modified the component palette to remove the search stuff.

--- taverna-tng/trunk/mockup3/uk.org.taverna.tng.mockup3.workbench/src/uk/org/taverna/tng/mockup3/workbench/views/ComponentPaletteView.java
+++ taverna-tng/trunk/mockup3/uk.org.taverna.tng.mockup3.workbench/src/uk/org/taverna/tng/mockup3/workbench/views/ComponentPaletteView.java
@@ -3,6 +3,7 @@ package uk.org.taverna.tng.mockup3.workbench.views;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.custom.StackLayout;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -23,64 +24,30 @@ public class ComponentPaletteView extends ViewPart implements ISearchTermProvide
 	protected static final int SASH_LIMIT = 50;
 	
 	private Composite parentContainer;
-	private SashForm containerSashForm;
-	private Button searchMoreButton;
+	private StackLayout containerStackLayout;
 	private FilteredTree currentComponentsTree;
-	private TreeViewer searchComponentsTree;
 
 	/**
 	 * This is a callback that will allow us to create the viewer and initialize
 	 * it.
 	 */
 	public void createPartControl(Composite parent) {
-//		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL
-//				| SWT.V_SCROLL);
-//		viewer.setContentProvider(new ViewContentProvider());
-//		viewer.setLabelProvider(new ViewLabelProvider());
-//		viewer.setInput(getViewSite());
-		
 		parentContainer = parent;
 		
-		final FillLayout mainLayout = new FillLayout(SWT.VERTICAL);
-		mainLayout.marginHeight = 0;
-		mainLayout.marginWidth = 0;
-		parent.setLayout(mainLayout);
+		containerStackLayout = new StackLayout();
 		
-		containerSashForm = new SashForm(parent, SWT.SMOOTH | SWT.VERTICAL);
-		
-		Composite top = new Composite(containerSashForm, SWT.NONE);
-		top.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		
-		GridLayout topLayout = new GridLayout(1, true);
-		topLayout.marginWidth = 0;
-		topLayout.marginHeight = 0;
-		top.setLayout(topLayout);
-		
-		currentComponentsTree = new FilteredTree(top, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL, new PatternFilter(), true);
+		parent.setLayoutData(new GridData(GridData.FILL_BOTH));
+ 		parent.setLayout(containerStackLayout);
+ 		
+ 		currentComponentsTree = new FilteredTree(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL, new PatternFilter(), true);
 		currentComponentsTree.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 		TreeViewer currentComponentsTreeViewer = currentComponentsTree.getViewer();
 		getSite().setSelectionProvider(currentComponentsTreeViewer);
 		currentComponentsTreeViewer.setContentProvider(new CustomContentProvider());
 		currentComponentsTreeViewer.setLabelProvider(new CustomLabelProvider());
 		currentComponentsTreeViewer.setInput(ComponentDefinitionsRegistry.INSTANCE.getAllEntries());
-		
-		Composite bottom = new Composite(containerSashForm, SWT.NONE);
-		bottom.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		
-		GridLayout bottomLayout = new GridLayout(1, true);
-		bottomLayout.marginWidth = 0;
-		bottomLayout.marginHeight = 0;
-		bottom.setLayout(bottomLayout);
-		
-		searchMoreButton = new Button(bottom, SWT.PUSH);
-		searchMoreButton.setText("Search for more...");
-		GridData searchMoreButtonGridData = new GridData(SWT.FILL, SWT.END, true, false);
-		searchMoreButton.setLayoutData(searchMoreButtonGridData);
-		
-		searchComponentsTree = new TreeViewer(bottom, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
-		searchComponentsTree.getControl().setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		
-		containerSashForm.setWeights(new int[]{2, 1});
+ 		
+ 		containerStackLayout.topControl = currentComponentsTree;
 	}
 
 	/**
