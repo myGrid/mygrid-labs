714683f9fc41e06ed5dba46f78e019f0d753a228
Jiten Bhagat <mail@jits.co.uk>
2010-07-22 13:45:30 +0000

    Added a Search and Details view (just the skeletons of).

---
+++ taverna-tng/trunk/mockup3/uk.org.taverna.tng.mockup3.workbench/src/uk/org/taverna/tng/mockup3/workbench/views/ComponentDetailsView.java
@@ -0,0 +1,24 @@
+package uk.org.taverna.tng.mockup3.workbench.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class ComponentDetailsView extends ViewPart {
+
+	public ComponentDetailsView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
