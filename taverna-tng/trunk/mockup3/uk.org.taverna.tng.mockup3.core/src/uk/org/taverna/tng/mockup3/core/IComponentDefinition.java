48622b050e8d816d7ee5c3e2239f3b4b7e19697f
Jiten Bhagat <mail@jits.co.uk>
2010-07-23 17:51:20 +0000

    Fleshed out IComponentDefinition.

--- taverna-tng/trunk/mockup3/uk.org.taverna.tng.mockup3.core/src/uk/org/taverna/tng/mockup3/core/IComponentDefinition.java
+++ taverna-tng/trunk/mockup3/uk.org.taverna.tng.mockup3.core/src/uk/org/taverna/tng/mockup3/core/IComponentDefinition.java
@@ -1,5 +1,10 @@
 package uk.org.taverna.tng.mockup3.core;
 
 public interface IComponentDefinition {
-
+	public String getExternalId();
+	public String getName();
+	public String getDescription();
+	public String getGroup();
+	public String getFamily();
+	public String getIcon();
 }
