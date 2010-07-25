633cbc05102be079501142c3e2502e7b26e3a1c8
sergejs@mygrid.org.uk <sergejs@mygrid.org.uk@d2a11066-01d3-11df-8b0e-05a2803efa1e>
2010-07-25 23:06:27 +0000

    XPath Activity: -- added option to include XML element attributes as nodes in the XML tree -- added option to include XML element / attribute textual values into the XML tree -- included namespaces into the display labels of elements / attributes in the XML tree -- added distinctive icons to the nodes in the XML tree

--- xpath/xpath-activity-ui/src/main/java/prototype/XPathActivityXMLTree.java
+++ xpath/xpath-activity-ui/src/main/java/prototype/XPathActivityXMLTree.java
@@ -1,19 +1,25 @@
 package prototype;
+import java.awt.Component;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTree;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.MutableTreeNode;
+import javax.swing.tree.TreeCellRenderer;
 import javax.swing.tree.TreePath;
 
+import org.dom4j.Attribute;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
+import org.dom4j.tree.DefaultAttribute;
 
 
 public class XPathActivityXMLTree extends JTree
@@ -29,12 +35,13 @@ public class XPathActivityXMLTree extends JTree
    */
   private XPathActivityConfigurationPanel parentConfigPanel;
   
-  
-  public XPathActivityXMLTree(MutableTreeNode root, XPathActivityConfigurationPanel parentConfigPanel)
+  public XPathActivityXMLTree(MutableTreeNode root, XPathActivityConfigurationPanel parentConfigPanel, boolean bIncludeElementValues)
   {
     super(root);
     this.parentConfigPanel = parentConfigPanel;
     this.currentXPathExpression = "";
+    
+    this.setCellRenderer(new XPathActivityXMLTreeRenderer(bIncludeElementValues));
     
     this.addTreeSelectionListener(new TreeSelectionListener() {
       public void valueChanged(TreeSelectionEvent e) {
@@ -50,24 +57,26 @@ public class XPathActivityXMLTree extends JTree
    * 
    * @param xmlData XML document in the form of a <code>String</code> to
    *        derive the tree from.
+   * @param bIncludeAttributesIntoTree
+   * @param bIncludeValuesIntoTree
    * @param parentConfigPanel
    * @return
    * @throws DocumentException if <code>xmlData</code> does not
    *                           contain a valid XML document. 
    * 
    */
-  public static XPathActivityXMLTree createFromXMLData(String xmlData,
-                   XPathActivityConfigurationPanel parentConfigPanel) throws DocumentException
+  public static XPathActivityXMLTree createFromXMLData(String xmlData, boolean bIncludeAttributesIntoTree,
+                   boolean bIncludeValuesIntoTree, XPathActivityConfigurationPanel parentConfigPanel) throws DocumentException
   {
     // try to parse the XML document - the next line will throw an exception if
     // the document is not well-formed; proceed otherwise
     Document doc = DocumentHelper.parseText(xmlData);
     
     Element rootEl = doc.getRootElement();
-    DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode(rootEl.getName());
-    populate(rootNode, rootEl.elements());
+    XPathActivityXMLTreeNode rootNode = new XPathActivityXMLTreeNode(rootEl, false);
+    populate(rootNode, rootEl, bIncludeAttributesIntoTree);
     
-    return (new XPathActivityXMLTree(rootNode, parentConfigPanel));
+    return (new XPathActivityXMLTree(rootNode, parentConfigPanel, bIncludeValuesIntoTree));
   }
   
   /**
@@ -76,16 +85,26 @@ public class XPathActivityXMLTree extends JTree
    * @param node
    * @param elements
    */
-  private static void populate(DefaultMutableTreeNode node, List<Element> elements)
+  private static void populate(DefaultMutableTreeNode node, Element element,
+                               boolean bIncludeAttributesIntoTree)
   {
-    Iterator<Element> elementIterator = elements.iterator();
+    Iterator<Element> elementIterator = element.elements().iterator();
     while (elementIterator.hasNext()) {
       Element childElement = elementIterator.next();
-      DefaultMutableTreeNode childNode = new DefaultMutableTreeNode(childElement.getName());
+      XPathActivityXMLTreeNode childNode = new XPathActivityXMLTreeNode(childElement, false);
       node.add(childNode);
       
       // recursively repeat for all children of the current child element
-      populate(childNode, childElement.elements());
+      populate(childNode, childElement, bIncludeAttributesIntoTree);
+    }
+    
+    
+    // add attributes of the element as its children, if necessary
+    if (bIncludeAttributesIntoTree) {
+      List<Attribute> attributes = element.attributes();
+      for (Attribute attribute : attributes) {
+        node.add(new XPathActivityXMLTreeNode(attribute, true));
+      }
     }
   }
   
@@ -116,11 +135,11 @@ public class XPathActivityXMLTree extends JTree
     StringBuilder xpath = new StringBuilder();
     TreePath parentPath = newSelectedPath;
     while (parentPath.getPathCount() > 1) {
-      xpath.insert(0, "/" + parentPath.getLastPathComponent());
+      xpath.insert(0, "/" + ((XPathActivityXMLTreeNode)parentPath.getLastPathComponent()).getTreeNodeDisplayLabel(false));
       parentPath = parentPath.getParentPath();
       this.addSelectionPath(parentPath);
     }
-    xpath.insert(0, "/" + parentPath.getLastPathComponent());
+    xpath.insert(0, "/" + ((XPathActivityXMLTreeNode)parentPath.getLastPathComponent()).getTreeNodeDisplayLabel(false));
     this.currentXPathExpression = xpath.toString();
     
     
@@ -147,5 +166,127 @@ public class XPathActivityXMLTree extends JTree
   
   public String getCurrentXPathExpression() {
     return currentXPathExpression;
+  }
+  
+  
+  
+  // ------------------------- XML Tree Nodes --------------------------
+  
+  
+  private static class XPathActivityXMLTreeNode extends DefaultMutableTreeNode
+  {
+    private final boolean isAttribute;
+
+    public XPathActivityXMLTreeNode(Object userObject, boolean isAttribute)
+    {
+      super(userObject);
+      this.isAttribute = isAttribute;
+    }
+    
+    public boolean isAttribute() {
+      return (isAttribute);
+    }
+    
+    public String getTreeNodeDisplayLabel(boolean bIncludeValue)
+    {
+      Object nodeUserObject = this.getUserObject();
+      
+      String nodeElementNamespacePrefix = null; 
+      String nodeElementTextValue = null;
+      StringBuilder nodeElementLabel = new StringBuilder();
+      
+      if (nodeUserObject instanceof Element)
+      {
+        Element enclosedElement = (Element)nodeUserObject;
+        
+        nodeElementLabel.append(enclosedElement.getName());
+        nodeElementNamespacePrefix = enclosedElement.getNamespacePrefix();
+        if (bIncludeValue) {
+          nodeElementTextValue = enclosedElement.getTextTrim();
+        }
+      }
+      else if (nodeUserObject instanceof Attribute)
+      {
+        Attribute enclodedAttribute = (Attribute)nodeUserObject;
+        
+        nodeElementLabel.append(enclodedAttribute.getName());
+        nodeElementNamespacePrefix = enclodedAttribute.getNamespacePrefix();
+        if (bIncludeValue) {
+          nodeElementTextValue = enclodedAttribute.getText();
+        }
+      }
+      
+      // add namespace prefix, if one is known
+      if (nodeElementNamespacePrefix != null && nodeElementNamespacePrefix.length() > 0) {
+        nodeElementLabel.insert(0, nodeElementNamespacePrefix + ":");
+      }
+      
+      // add element value, if necessary
+      if (nodeElementTextValue != null && nodeElementTextValue.length() > 0) {
+        // TODO - truncate to MAX_LENGTH
+        // TODO - remove all blank lines...
+        nodeElementLabel.append(" - " + nodeElementTextValue);
+      }
+    
+      return (nodeElementLabel.toString());
+    }
+  }
+  
+  
+  
+  // ----------------------- Tree Cell Renderer --------------------------
+  
+  /**
+   * 
+   * @author Sergejs Aleksejevs
+   */
+  private class XPathActivityXMLTreeRenderer extends DefaultTreeCellRenderer
+  {
+    private boolean bIncludeElementValues;
+    
+    public XPathActivityXMLTreeRenderer(boolean bIncludeElementValues) {
+      super();
+      this.bIncludeElementValues = bIncludeElementValues;
+    }
+    
+    
+    public Component getTreeCellRendererComponent(JTree tree, Object value,
+        boolean selected, boolean expanded, boolean leaf, int row,
+        boolean hasFocus)
+    {
+      // obtain the default rendering, we'll then customize it
+      Component defaultRendering = super.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus);
+      
+      // it is most likely that the default rendering will be a JLabel, check just to be safe
+      if (defaultRendering instanceof JLabel)
+      {
+        JLabel defaultRenderedLabel = ((JLabel)defaultRendering);
+        
+        // ---------- CHOOSE APPROPRIATE ICON FOR THE NODE ------------
+        if (row == 0) {
+          // set the icon for the XML tree root node
+          defaultRenderedLabel.setIcon(XPathActivityIcon.getIconById(XPathActivityIcon.XML_TREE_ROOT_ICON));
+        }
+        else {
+          // set the icon for the XML tree node
+          if (value instanceof XPathActivityXMLTreeNode && 
+              ((XPathActivityXMLTreeNode)value).isAttribute())
+          {
+            defaultRenderedLabel.setIcon(XPathActivityIcon.getIconById(XPathActivityIcon.XML_TREE_ATTRIBUTE_ICON));
+          }
+          else {
+            defaultRenderedLabel.setIcon(XPathActivityIcon.getIconById(XPathActivityIcon.XML_TREE_NODE_ICON));
+          }
+        }
+        
+        
+        // ----------- CHOOSE THE DISPLAY TITLE FOR THE NODE ------------
+        if (value instanceof XPathActivityXMLTreeNode) {
+          defaultRenderedLabel.setText(((XPathActivityXMLTreeNode)value).getTreeNodeDisplayLabel(this.bIncludeElementValues));
+        }
+      }
+      
+      return (defaultRendering);
+    }
   }
 }
