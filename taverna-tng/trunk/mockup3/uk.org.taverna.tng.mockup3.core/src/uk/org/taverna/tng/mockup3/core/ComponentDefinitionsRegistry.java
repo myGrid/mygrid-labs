f8013648dad486c890f287e5a806aacbe00732bf
jits@mygrid.org.uk <jits@mygrid.org.uk@d2a11066-01d3-11df-8b0e-05a2803efa1e>
2010-07-26 15:06:38 +0000

    Work on the component definitions registry and getting these loaded up into the components palette.

--- taverna-tng/trunk/mockup3/uk.org.taverna.tng.mockup3.core/src/uk/org/taverna/tng/mockup3/core/ComponentDefinitionsRegistry.java
+++ taverna-tng/trunk/mockup3/uk.org.taverna.tng.mockup3.core/src/uk/org/taverna/tng/mockup3/core/ComponentDefinitionsRegistry.java
@@ -8,11 +8,15 @@ import org.eclipse.core.runtime.Platform;
 
 public class ComponentDefinitionsRegistry {
 
-	private static final String EXTENSION_POINT_ID = "uk.org.taverna.tng.mockup3.core.componentDefinitions";
+	private static final String EXTENSION_POINT_NAMESPACE = "uk.org.taverna.tng.mockup3.core";
+	private static final String EXTENSION_POINT_NAME = "workflowComponents";
+
+	private static final String MISC_GROUP = "Misc";
 
 	public final static ComponentDefinitionsRegistry INSTANCE = new ComponentDefinitionsRegistry();
 
 	private List<IComponentDefinition> entries;
+	private List<ComponentDefinitionGroup> topLevelGroups;
 
 	private ComponentDefinitionsRegistry() {
 	}
@@ -24,33 +28,139 @@ public class ComponentDefinitionsRegistry {
 
 		return entries;
 	}
-	
+
+	public List<ComponentDefinitionGroup> getTopLevelGroups() {
+		if (topLevelGroups == null) {
+			reloadAll();
+		}
+
+		return topLevelGroups;
+	}
+
 	public void addComponentDefinition(IComponentDefinition def) {
-		if (this.entries != null && def != null) {
-			this.entries.add(def);
+		if (entries != null && def != null) {
+			entries.add(def);
+			// TODO: update the topLevelGroups collection to reflect the
+			// addition.
 		}
 	}
-	
+
 	public void addComponentDefinition(IConfigurationElement element) {
-		if (this.entries != null && element != null) {
-			this.entries.add(new ComponentDefinition(element));
+		if (entries != null && element != null) {
+			entries.add(new ComponentDefinition(element));
+			// TODO: update the topLevelGroups collection to reflect the
+			// addition.
 		}
 	}
-	
+
 	private void reloadAll() {
-		this.entries = new ArrayList<IComponentDefinition>();
-		this.loadFromExtensionsRegistry();	
+		entries = new ArrayList<IComponentDefinition>();
+		topLevelGroups = new ArrayList<ComponentDefinitionGroup>();
+		loadFromExtensionsRegistry();
+		groupTheDefinitions();
 	}
 
 	private void loadFromExtensionsRegistry() {
 		IConfigurationElement[] elements = Platform.getExtensionRegistry()
-				.getConfigurationElementsFor(EXTENSION_POINT_ID);
+				.getConfigurationElementsFor(EXTENSION_POINT_NAMESPACE,
+						EXTENSION_POINT_NAME);
+
+		System.out.println(elements.length
+				+ " extensions loaded from the Extensions Registry");
 
 		for (IConfigurationElement element : elements) {
-			this.entries.add(new ComponentDefinition(element));
+			System.out.println(element.getName());
+			if (element.getName().equals("componentDefinition")) {
+				entries.add(new ComponentDefinition(element));
+			}
+		}
+
+		System.out
+				.println("INFO: "
+						+ entries.size()
+						+ " Component Definitions were found from the Extensions Registry");
+	}
+
+	private void groupTheDefinitions() {
+		if (entries != null) {
+			for (IComponentDefinition definition : entries) {
+				addDefinitionToGroups(definition);
+			}
+		}
+		
+		System.out.println("INFO: " + topLevelGroups.size() + " top level component definition groups found"); 
+	}
+
+	private void addDefinitionToGroups(IComponentDefinition definition) {
+		ComponentDefinitionGroup groupToGoIn = getComponentDefinitionsGroup(definition
+				.getGroup());
+		groupToGoIn.addChildDefinition(definition);
+	}
+
+	public ComponentDefinitionGroup getComponentDefinitionsGroup(
+			String groupPath) {
+		String[] constituentGroups = splitPath(groupPath);
+
+		if (constituentGroups.length == 0) {
+			// Get or create the "Misc" top level group
+			ComponentDefinitionGroup group = ComponentDefinitionGroup
+					.findWithin(topLevelGroups, MISC_GROUP, 1);
+			if (group == null) {
+				group = new ComponentDefinitionGroup(MISC_GROUP);
+				topLevelGroups.add(group);
+			}
+			return group;
+		} else {
+			ComponentDefinitionGroup group;
+
+			List<ComponentDefinitionGroup> currentGroupListToProcess = topLevelGroups;
+			ComponentDefinitionGroup currentGroupToProcess = null;
+
+			ComponentDefinitionGroup currentParentGroup = null;
+
+			for (String groupName : constituentGroups) {
+				currentGroupToProcess = ComponentDefinitionGroup.findWithin(
+						currentGroupListToProcess, groupName, 1);
+
+				if (currentGroupToProcess == null) {
+					currentGroupToProcess = new ComponentDefinitionGroup(
+							groupName, currentParentGroup);
+					currentGroupListToProcess.add(currentGroupToProcess);
+				}
+
+				currentGroupListToProcess = currentGroupToProcess
+						.getChildGroups();
+
+				currentParentGroup = currentGroupToProcess;
+			}
+
+			group = currentGroupToProcess;
+
+			if (group == null) {
+				throw new RuntimeException("group should not be null by now!");
+			}
+
+			return group;
 		}
 	}
-	
-	// TODO: implement a registry change listener so that when new components 
+
+	private String[] splitPath(String path) {
+		if (path == null) {
+			return new String[0];
+		} else {
+			String[] items = path.split("/");
+			ArrayList<String> finalItems = new ArrayList<String>();
+			for (String s : items) {
+				if (s != null && !s.equals("")) {
+					finalItems.add(s);
+				}
+			}
+			return finalItems.toArray(new String[finalItems.size()]);
+		}
+		// TODO: add checks for empty / null groups names in the output
+		// collection
+	}
+
+	// TODO: implement a registry change listener so that when new components
 	// are added via Eclipse plug-ins they can be added to this manager!
 }
--- taverna-tng/trunk/mockup3/uk.org.taverna.tng.mockup3.workbench/src/uk/org/taverna/tng/mockup3/workbench/util/AdapterFactory.java
+++ taverna-tng/trunk/mockup3/uk.org.taverna.tng.mockup3.workbench/src/uk/org/taverna/tng/mockup3/workbench/util/AdapterFactory.java
@@ -7,9 +7,15 @@ import java.util.List;
 import org.eclipse.core.runtime.IAdapterFactory;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.model.IWorkbenchAdapter;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+import uk.org.taverna.tng.mockup3.core.ComponentDefinitionGroup;
+import uk.org.taverna.tng.mockup3.core.ComponentDefinitionsRegistry;
+import uk.org.taverna.tng.mockup3.core.IComponentDefinition;
+import uk.org.taverna.tng.mockup3.workbench.Application;
 
 public class AdapterFactory implements IAdapterFactory {
-	
+
 	private IWorkbenchAdapter componentSearchResultsAdapter = new IWorkbenchAdapter() {
 		public Object getParent(Object o) {
 			return null;
@@ -18,7 +24,7 @@ public class AdapterFactory implements IAdapterFactory {
 		public String getLabel(Object o) {
 			return "";
 		}
-		
+
 		public ImageDescriptor getImageDescriptor(Object o) {
 			return null;
 		}
@@ -30,7 +36,7 @@ public class AdapterFactory implements IAdapterFactory {
 			return output.toArray();
 		}
 	};
-	
+
 	private IWorkbenchAdapter componentSearchSourceResultsAdapter = new IWorkbenchAdapter() {
 		public Object getParent(Object o) {
 			ComponentSearchSourceResults results = (ComponentSearchSourceResults) o;
@@ -39,9 +45,10 @@ public class AdapterFactory implements IAdapterFactory {
 
 		public String getLabel(Object o) {
 			ComponentSearchSourceResults results = (ComponentSearchSourceResults) o;
-			return results.getSource().getName() + " (" + results.getResults().size() + ")";
+			return results.getSource().getName() + " ("
+					+ results.getResults().size() + ")";
 		}
-		
+
 		public ImageDescriptor getImageDescriptor(Object o) {
 			ComponentSearchSourceResults results = (ComponentSearchSourceResults) o;
 			return results.getSource().getIcon();
@@ -54,7 +61,7 @@ public class AdapterFactory implements IAdapterFactory {
 			return output.toArray();
 		}
 	};
-	
+
 	private IWorkbenchAdapter componentSearchResultItemAdapter = new IWorkbenchAdapter() {
 		public Object getParent(Object o) {
 			ComponentSearchResultItem item = (ComponentSearchResultItem) o;
@@ -65,7 +72,7 @@ public class AdapterFactory implements IAdapterFactory {
 			ComponentSearchResultItem item = (ComponentSearchResultItem) o;
 			return item.getName();
 		}
-		
+
 		public ImageDescriptor getImageDescriptor(Object o) {
 			ComponentSearchResultItem item = (ComponentSearchResultItem) o;
 			return item.getIcon();
@@ -75,7 +82,78 @@ public class AdapterFactory implements IAdapterFactory {
 			return new Object[0];
 		}
 	};
+
+	private IWorkbenchAdapter componentDefinitionsRegistryAdapter = new IWorkbenchAdapter() {
+		public Object getParent(Object o) {
+			return null;
+		}
+
+		public String getLabel(Object o) {
+			return "";
+		}
+
+		public ImageDescriptor getImageDescriptor(Object o) {
+			return null;
+		}
+
+		public Object[] getChildren(Object o) {
+			ComponentDefinitionsRegistry registry = (ComponentDefinitionsRegistry) o;
+			List<ComponentDefinitionGroup> output = new ArrayList<ComponentDefinitionGroup>();
+			collect(registry.getTopLevelGroups().iterator(), output);
+			return output.toArray();
+		}
+	};
+
+	private IWorkbenchAdapter componentDefinitionGroupAdapter = new IWorkbenchAdapter() {
+		public Object getParent(Object o) {
+			ComponentDefinitionGroup group = (ComponentDefinitionGroup) o;
+			return group.getParent();
+		}
+
+		public String getLabel(Object o) {
+			ComponentDefinitionGroup group = (ComponentDefinitionGroup) o;
+			return group.getName();
+		}
+
+		public ImageDescriptor getImageDescriptor(Object o) {
+			return AbstractUIPlugin.imageDescriptorFromPlugin(Application.PLUGIN_ID,
+					IImageKeys.FOLDER_ICON);
+		}
+
+		public Object[] getChildren(Object o) {
+			ComponentDefinitionGroup group = (ComponentDefinitionGroup) o;
+			List<ComponentDefinitionGroup> output1 = new ArrayList<ComponentDefinitionGroup>();
+			List<IComponentDefinition> output2 = new ArrayList<IComponentDefinition>();
+			collect(group.getChildGroups().iterator(), output1);
+			collect(group.getChildDefinitions().iterator(), output2);
+			ArrayList output3 = new ArrayList(output1);
+			output3.addAll(output2);
+			return output3.toArray();
+		}
+	};
 	
+	private IWorkbenchAdapter componentDefinitionAdapter = new IWorkbenchAdapter() {
+		public Object getParent(Object o) {
+			IComponentDefinition item = (IComponentDefinition) o;
+			return null;
+		}
+
+		public String getLabel(Object o) {
+			IComponentDefinition item = (IComponentDefinition) o;
+			return item.getName();
+		}
+
+		public ImageDescriptor getImageDescriptor(Object o) {
+			IComponentDefinition item = (IComponentDefinition) o;
+			return AbstractUIPlugin.imageDescriptorFromPlugin(Application.PLUGIN_ID,
+					item.getIcon());
+		}
+
+		public Object[] getChildren(Object o) {
+			return new Object[0];
+		}
+	};
+
 	@Override
 	public Object getAdapter(Object adaptableObject, Class adapterType) {
 		if (adapterType == IWorkbenchAdapter.class
@@ -87,6 +165,15 @@ public class AdapterFactory implements IAdapterFactory {
 		if (adapterType == IWorkbenchAdapter.class
 				&& adaptableObject instanceof ComponentSearchResultItem)
 			return componentSearchResultItemAdapter;
+		if (adapterType == IWorkbenchAdapter.class
+				&& adaptableObject instanceof ComponentDefinitionsRegistry)
+			return componentDefinitionsRegistryAdapter;
+		if (adapterType == IWorkbenchAdapter.class
+				&& adaptableObject instanceof ComponentDefinitionGroup)
+			return componentDefinitionGroupAdapter;
+		if (adapterType == IWorkbenchAdapter.class
+				&& adaptableObject instanceof IComponentDefinition)
+			return componentDefinitionAdapter;
 		return null;
 	}
 
@@ -94,7 +181,7 @@ public class AdapterFactory implements IAdapterFactory {
 	public Class[] getAdapterList() {
 		return new Class[] { IWorkbenchAdapter.class };
 	}
-	
+
 	private void collect(Iterator itr, List list) {
 		while (itr.hasNext())
 			list.add(itr.next());
