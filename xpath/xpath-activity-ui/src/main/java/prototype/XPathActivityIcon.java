5b24cda6bb3351419ffb1311fc6632e138a16872
sergejs@mygrid.org.uk <sergejs@mygrid.org.uk@d2a11066-01d3-11df-8b0e-05a2803efa1e>
2010-07-26 13:05:20 +0000

    XPath Activity: -- fixed the layout of the config panel: previously, right-hand side pane with the XML tree was getting more space, now it's equally split -- added an icon for "parse example XML" button

--- xpath/xpath-activity-ui/src/main/java/prototype/XPathActivityIcon.java
+++ xpath/xpath-activity-ui/src/main/java/prototype/XPathActivityIcon.java
@@ -14,12 +14,16 @@ public class XPathActivityIcon implements ActivityIconSPI
   private static final String FAMFAMFAM_SILK_PATH = "famfamfam_silk";
   
   public static final String XPATH_ACTIVITY_ICON = FAMFAMFAM_SILK_PATH + "/" + "page_white_code.png";
+  public static final String XPATH_ACTIVITY_CONFIGURATION_PARSE_XML_ICON = "arrow_right.png";
+  
   public static final String XML_TREE_ROOT_ICON = FAMFAMFAM_SILK_PATH + "/" + "page_white_code.png";
   public static final String XML_TREE_NODE_ICON = FAMFAMFAM_SILK_PATH + "/" + "tag.png";
-  public static final String XML_TREE_ATTRIBUTE_ICON = "xpath_attribute.gif";
+  public static final String XML_TREE_ATTRIBUTE_ICON = "xpath_attribute.png";
+  
   public static final String XPATH_STATUS_OK_ICON = FAMFAMFAM_SILK_PATH + "/" + "accept.png";
   public static final String XPATH_STATUS_ERROR_ICON = FAMFAMFAM_SILK_PATH + "/" + "exclamation.png";
   public static final String XPATH_STATUS_UNKNOWN_ICON = FAMFAMFAM_SILK_PATH + "/" + "help.png";
+  
   
   
   private static ImageIcon icon;
