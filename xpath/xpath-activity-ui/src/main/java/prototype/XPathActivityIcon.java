3fb7f5b257c74b8443e04d33c36a4c58a5cb9020
sergejs@mygrid.org.uk <sergejs@mygrid.org.uk@d2a11066-01d3-11df-8b0e-05a2803efa1e>
2010-07-28 09:00:32 +0000

    XPath Activity - added contextual menu to expand/collapse the XML tree

--- xpath/xpath-activity-ui/src/main/java/prototype/XPathActivityIcon.java
+++ xpath/xpath-activity-ui/src/main/java/prototype/XPathActivityIcon.java
@@ -11,18 +11,21 @@ import net.sf.taverna.t2.workflowmodel.processor.activity.Activity;
  */
 public class XPathActivityIcon implements ActivityIconSPI
 {
-  private static final String FAMFAMFAM_SILK_PATH = "famfamfam_silk";
+  private static final String FAMFAMFAM_SILK_PATH = "famfamfam_silk/";
   
-  public static final String XPATH_ACTIVITY_ICON = FAMFAMFAM_SILK_PATH + "/" + "page_white_code.png";
+  public static final String XPATH_ACTIVITY_ICON = FAMFAMFAM_SILK_PATH + "page_white_code.png";
   public static final String XPATH_ACTIVITY_CONFIGURATION_PARSE_XML_ICON = "arrow_right.png";
   
-  public static final String XML_TREE_ROOT_ICON = FAMFAMFAM_SILK_PATH + "/" + "page_white_code.png";
-  public static final String XML_TREE_NODE_ICON = FAMFAMFAM_SILK_PATH + "/" + "tag.png";
+  public static final String XML_TREE_ROOT_ICON = FAMFAMFAM_SILK_PATH + "page_white_code.png";
+  public static final String XML_TREE_NODE_ICON = FAMFAMFAM_SILK_PATH + "tag.png";
   public static final String XML_TREE_ATTRIBUTE_ICON = "xpath_attribute.png";
   
-  public static final String XPATH_STATUS_OK_ICON = FAMFAMFAM_SILK_PATH + "/" + "accept.png";
-  public static final String XPATH_STATUS_ERROR_ICON = FAMFAMFAM_SILK_PATH + "/" + "exclamation.png";
-  public static final String XPATH_STATUS_UNKNOWN_ICON = FAMFAMFAM_SILK_PATH + "/" + "help.png";
+  public static final String XML_TREE_EXPAND_ALL_ICON = FAMFAMFAM_SILK_PATH + "text_linespacing.png";
+  public static final String XML_TREE_COLLAPSE_ALL_ICON = FAMFAMFAM_SILK_PATH + "text_linespacing (collapse).png";
+  
+  public static final String XPATH_STATUS_OK_ICON = FAMFAMFAM_SILK_PATH + "accept.png";
+  public static final String XPATH_STATUS_ERROR_ICON = FAMFAMFAM_SILK_PATH + "exclamation.png";
+  public static final String XPATH_STATUS_UNKNOWN_ICON = FAMFAMFAM_SILK_PATH + "help.png";
   
   
   
