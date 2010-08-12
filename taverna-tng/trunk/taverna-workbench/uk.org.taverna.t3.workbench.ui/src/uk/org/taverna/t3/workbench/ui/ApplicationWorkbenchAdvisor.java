6b4e25ef651634337e8771f1fb80616d709a025b
Jiten Bhagat <mail@jits.co.uk>
2010-08-12 17:28:51 +0000

    Basic plugins architecture for the next gen taverna-workbench.
    This defines a top level plugin project (uk.org.taverna.t3.workbench) which defines the build target definition (that MUST be applied to your workspace) and also the main product definition - taverna-workbench.product - that defines the product to run/debug.
    
    This product definition is the main starting point and is "features" based (ie: features are added to the product, not plugins). The uk.org.taverna.t3.feature project is currently used in this product, and is the base feature for the next gen workbench.

---
+++ taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,18 @@
+package uk.org.taverna.t3.workbench.ui;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "uk.org.taverna.t3.workbench.ui.perspective"; //$NON-NLS-1$
+
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+}
