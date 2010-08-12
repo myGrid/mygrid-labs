190298fe60e106d7b470cfdaa8ec8381578d51b4
jits@mygrid.org.uk <jits@mygrid.org.uk@d2a11066-01d3-11df-8b0e-05a2803efa1e>
2010-08-12 17:28:51 +0000

    Basic plugins architecture for the next gen taverna-workbench.
    This defines a top level plugin project (uk.org.taverna.t3.workbench) which defines the build target definition (that MUST be applied to your workspace) and also the main product definition - taverna-workbench.product - that defines the product to run/debug.
    
    This product definition is the main starting point and is "features" based (ie: features are added to the product, not plugins). The uk.org.taverna.t3.feature project is currently used in this product, and is the base feature for the next gen workbench.

---
+++ taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/Perspective.java
@@ -0,0 +1,10 @@
+package uk.org.taverna.t3.workbench.ui;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
