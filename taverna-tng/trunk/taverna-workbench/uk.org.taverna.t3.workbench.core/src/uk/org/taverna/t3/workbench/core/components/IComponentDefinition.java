cc507a6213b57c02dc116975b2a38a5738f57755
Jiten Bhagat <mail@jits.co.uk>
2010-08-17 16:19:34 +0000

    Added some core interfaces and a Util class

---
+++ taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.core/src/uk/org/taverna/t3/workbench/core/components/IComponentDefinition.java
@@ -0,0 +1,10 @@
+package uk.org.taverna.t3.workbench.core.components;
+
+public interface IComponentDefinition {
+	public String getExternalId();
+	public String getName();
+	public String getDescription();
+	public String getGroup();
+	public String getFamily();
+	public String getIcon();
+}
