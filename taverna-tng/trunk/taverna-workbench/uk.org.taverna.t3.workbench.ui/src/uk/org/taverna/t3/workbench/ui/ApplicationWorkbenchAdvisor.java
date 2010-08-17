6f7ceca2c1c8110525c99d5b93932b51fd2424db
jits@mygrid.org.uk <jits@mygrid.org.uk@d2a11066-01d3-11df-8b0e-05a2803efa1e>
2010-08-17 16:38:07 +0000

    Various refactorings for the UI project

--- taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/ApplicationWorkbenchAdvisor.java
+++ taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/ApplicationWorkbenchAdvisor.java
@@ -4,15 +4,15 @@ import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
-public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+import uk.org.taverna.t3.workbench.ui.perspectives.DesignPerspective;
 
-	private static final String PERSPECTIVE_ID = "uk.org.taverna.t3.workbench.ui.perspective"; //$NON-NLS-1$
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
     public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
         return new ApplicationWorkbenchWindowAdvisor(configurer);
     }
 
 	public String getInitialWindowPerspectiveId() {
-		return PERSPECTIVE_ID;
+		return DesignPerspective.ID;
 	}
 }
---
+++ taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/perspectives/DesignPerspective.java
@@ -0,0 +1,12 @@
+package uk.org.taverna.t3.workbench.ui.perspectives;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class DesignPerspective implements IPerspectiveFactory {
+	
+	public static final String ID = "uk.org.taverna.t3.workbench.ui.perspectives.design";	//$NON-NLS-1$
+
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
