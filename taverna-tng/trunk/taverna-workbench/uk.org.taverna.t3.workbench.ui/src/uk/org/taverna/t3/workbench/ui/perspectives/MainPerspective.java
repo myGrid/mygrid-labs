eec7679096991cf8570d22957df7d71507ad0dce
jits@mygrid.org.uk <jits@mygrid.org.uk@d2a11066-01d3-11df-8b0e-05a2803efa1e>
2010-11-04 20:37:39 +0000

    Brought back (from the dead) the previous source code that somehow got deleted (!)

---
+++ taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/perspectives/MainPerspective.java
@@ -0,0 +1,12 @@
+package uk.org.taverna.t3.workbench.ui.perspectives;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class MainPerspective implements IPerspectiveFactory {
+	
+	public static final String ID = "uk.org.taverna.t3.workbench.ui.perspectives.main";	//$NON-NLS-1$
+
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
