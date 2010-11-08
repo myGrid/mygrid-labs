dd5c1aa12a65705d271405ff494b6cc1ff9f0578
Jiten Bhagat <mail@jits.co.uk>
2010-11-08 18:06:06 +0000

    - Added Lombok as a dependency. - Slight change to target platform def to see if it works on Mac.

--- taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/ApplicationWorkbenchAdvisor.java
+++ taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/ApplicationWorkbenchAdvisor.java
@@ -4,6 +4,7 @@ import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
+import uk.org.taverna.t3.workbench.components.definitions.model.ComponentDefinition;
 import uk.org.taverna.t3.workbench.ui.perspectives.MainPerspective;
 
 public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
