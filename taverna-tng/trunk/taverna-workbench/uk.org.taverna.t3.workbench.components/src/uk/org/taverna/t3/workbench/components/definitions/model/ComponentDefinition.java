6bdd958d6b32d0144792240eb3e5a711c62a2560
jits@mygrid.org.uk <jits@mygrid.org.uk@d2a11066-01d3-11df-8b0e-05a2803efa1e>
2010-11-08 10:45:46 +0000

    - Updated manifests to always use latest versions of uk.org.taverna.t3.workbench.* bundles. - Moved the ComponentDefinition class into a .model sub package.

--- taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.components/src/uk/org/taverna/t3/workbench/components/definitions/model/ComponentDefinition.java
+++ taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.components/src/uk/org/taverna/t3/workbench/components/definitions/model/ComponentDefinition.java
@@ -1,4 +1,4 @@
-package uk.org.taverna.t3.workbench.components.definitions;
+package uk.org.taverna.t3.workbench.components.definitions.model;
 
 public class ComponentDefinition {
 	//@SerializedName("dc:about")
