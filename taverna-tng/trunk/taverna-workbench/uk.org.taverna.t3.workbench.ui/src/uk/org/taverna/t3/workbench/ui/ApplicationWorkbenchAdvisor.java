6808f8857273a7c03538cbdc379a413d58b8a954
Jiten Bhagat <mail@jits.co.uk>
2010-11-09 11:19:48 +0000

    Added the .canvas.diagram project into the main product's dependencies.

--- taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/ApplicationWorkbenchAdvisor.java
+++ taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/ApplicationWorkbenchAdvisor.java
@@ -4,7 +4,6 @@ import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
-import uk.org.taverna.t3.workbench.components.definitions.model.ComponentDefinition;
 import uk.org.taverna.t3.workbench.ui.perspectives.MainPerspective;
 
 public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
