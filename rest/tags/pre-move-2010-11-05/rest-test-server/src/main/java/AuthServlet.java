00961dccebeb7b9aeed1dab14ec4d2d641f3ea97
Alan R Williams <alan.r.williams@manchester.ac.uk>
2010-11-09 11:58:11 +0000

    Moving into Taverna

---
+++ rest/tags/pre-move-2010-11-05/rest-test-server/src/main/java/AuthServlet.java
@@ -0,0 +1,47 @@
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+public class AuthServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+  public AuthServlet() {
+      // TODO Auto-generated constructor stub
+  }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	  ServerResponseGenerator.generateResponse(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	  ServerResponseGenerator.generateResponse(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	  ServerResponseGenerator.generateResponse(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	  ServerResponseGenerator.generateResponse(request, response);
+	}
+
+}
