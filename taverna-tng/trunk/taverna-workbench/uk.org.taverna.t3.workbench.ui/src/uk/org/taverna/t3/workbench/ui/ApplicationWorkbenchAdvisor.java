d1d592d7395cd7f7174332760f4ce2839523e603
jits@mygrid.org.uk <jits@mygrid.org.uk@d2a11066-01d3-11df-8b0e-05a2803efa1e>
2010-11-11 14:58:49 +0000

    CSS theming is now possible! (Currently only same basic styles).

--- taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/ApplicationWorkbenchAdvisor.java
+++ taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/ApplicationWorkbenchAdvisor.java
@@ -1,12 +1,19 @@
 package uk.org.taverna.t3.workbench.ui;
 
+import org.eclipse.ui.application.IWorkbenchConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
 import uk.org.taverna.t3.workbench.ui.perspectives.MainPerspective;
+import uk.org.taverna.t3.workbench.ui.themes.ThemeHelper;
 
 public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+	
+	public void initialize(IWorkbenchConfigurer configurer) {
+		super.initialize(configurer);
+		ThemeHelper.getEngine().setTheme("uk.org.taverna.t3.workbench.ui.css.themes.default", false);
+	}
 
     public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
         return new ApplicationWorkbenchWindowAdvisor(configurer);
