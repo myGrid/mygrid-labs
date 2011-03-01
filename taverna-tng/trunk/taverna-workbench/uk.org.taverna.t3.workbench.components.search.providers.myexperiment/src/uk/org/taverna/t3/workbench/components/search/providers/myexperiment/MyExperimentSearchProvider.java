674a9e6bd08241e24743d71b2452c403b30c4072
jits@mygrid.org.uk <jits@mygrid.org.uk@d2a11066-01d3-11df-8b0e-05a2803efa1e>
2011-03-01 13:41:31 +0000

    Remove Taverna Platform test code

---
+++ taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.components.search.providers.myexperiment/src/uk/org/taverna/t3/workbench/components/search/providers/myexperiment/MyExperimentSearchProvider.java
@@ -0,0 +1,35 @@
+package uk.org.taverna.t3.workbench.components.search.providers.myexperiment;
+
+import java.util.Map;
+
+import uk.org.taverna.t3.workbench.components.search.AbstractComponentSearchProvider;
+import uk.org.taverna.t3.workbench.components.search.ComponentSearchResults;
+
+public class MyExperimentSearchProvider extends AbstractComponentSearchProvider {
+
+	public MyExperimentSearchProvider(String label, String name,
+			String description, String baseUrl,
+			Map<String, Object> additionalConfig) {
+		super(label, name, description, baseUrl, additionalConfig);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getIconRef() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ComponentSearchResults search(String query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
