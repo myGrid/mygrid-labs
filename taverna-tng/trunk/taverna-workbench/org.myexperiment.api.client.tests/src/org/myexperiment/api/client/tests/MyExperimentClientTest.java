31eec33921c7eebac5c11d525ee6b5bb273e9f7d
jits@mygrid.org.uk <jits@mygrid.org.uk@d2a11066-01d3-11df-8b0e-05a2803efa1e>
2011-03-01 17:29:07 +0000

    Test fragment project for the myExp API Client bundle

---
+++ taverna-tng/trunk/taverna-workbench/org.myexperiment.api.client.tests/src/org/myexperiment/api/client/tests/MyExperimentClientTest.java
@@ -0,0 +1,38 @@
+package org.myexperiment.api.client.tests;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.jdom.Document;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.myexperiment.api.client.MyExperimentClient;
+import org.myexperiment.api.client.model.SearchEngine.QuerySearchInstance;
+
+import junit.framework.TestCase;
+
+public class MyExperimentClientTest extends TestCase {
+	
+	public final static String BASE_URL = "http://www.myexperiment.org";
+	
+	@Before
+	public void setup(){
+		
+	}
+	
+	@After
+	public void teardown() {
+		
+	}
+	
+	@Test
+	public void testSearch(){
+		MyExperimentClient client = new MyExperimentClient(BASE_URL);
+		QuerySearchInstance searchQuery = new QuerySearchInstance("blast", 100, true, true, true, true, true);
+		Document document = client.searchByQuery(searchQuery);
+		assertNotNull(document);
+	}
+	
+}
