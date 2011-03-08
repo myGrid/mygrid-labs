ba3ec07c59d402a43ea9a241db55bbecf311d3db
Jiten Bhagat <mail@jits.co.uk>
2011-03-08 17:39:13 +0000

    Load up more services from the Taverna Platform

--- taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/handlers/DevelopmentDebugHandler.java
+++ taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/handlers/DevelopmentDebugHandler.java
@@ -2,12 +2,16 @@ package uk.org.taverna.t3.workbench.ui.handlers;
 
 import java.net.URI;
 
+import net.sf.taverna.t2.reference.ReferenceService;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 
 import uk.org.taverna.platform.activity.ActivityService;
+import uk.org.taverna.platform.execution.api.ExecutionService;
+import uk.org.taverna.platform.run.api.RunService;
 import uk.org.taverna.t3.workbench.ui.util.Services;
 
 public class DevelopmentDebugHandler extends AbstractHandler implements
@@ -20,8 +24,8 @@ public class DevelopmentDebugHandler extends AbstractHandler implements
 		StringBuilder sb = new StringBuilder();
 		
 		// ActivityService
-		ActivityService activityService = Services.getInstance().getActivityService();
 		sb.append("\nProbing ActivityService...");
+		ActivityService activityService = Services.getInstance().getActivityService();
 		if (activityService == null) {
 			sb.append("\n\tActivityService currently not available! (is null)");
 		} else {
@@ -30,6 +34,36 @@ public class DevelopmentDebugHandler extends AbstractHandler implements
 				sb.append("\n\t\tTaverna Activity found: " + uri.getPath());
 			}
 		}
+		
+		// ReferenceService
+		sb.append("\nProbing ReferenceService");
+		ReferenceService referenceService = Services.getInstance().getReferenceService();
+		if (referenceService == null) {
+			sb.append("\n\tReferenceService currently not available! (is null)");
+		} else {
+			
+		}
+		
+		
+		// RunService
+		sb.append("\nProbing RunService");
+		RunService runService = Services.getInstance().getRunService();
+		if (runService == null) {
+			sb.append("\n\tRunService currently not available! (is null)");
+		} else {
+			
+		}
+		
+		// ExecutionService
+		sb.append("\nProbing ExecutionService");
+		ExecutionService executionService = Services.getInstance().getExecutionService();
+		if (executionService == null) {
+			sb.append("\n\tExecutionService currently not available! (is null)");
+		} else {
+			
+		}
+		
+		sb.append("\n");
 		
 		System.out.println(sb.toString());
 		
--- taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/util/ServicesConsumer.java
+++ taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/util/ServicesConsumer.java
@@ -1,6 +1,9 @@
 package uk.org.taverna.t3.workbench.ui.util;
 
+import net.sf.taverna.t2.reference.ReferenceService;
 import uk.org.taverna.platform.activity.ActivityService;
+import uk.org.taverna.platform.execution.api.ExecutionService;
+import uk.org.taverna.platform.run.api.RunService;
 
 public class ServicesConsumer {
 	
@@ -12,5 +15,35 @@ public class ServicesConsumer {
 	public void unsetActivityService(ActivityService service) {
 		System.out.println("ActivityService was unset. Why did you do this to me?");
 		Services.getInstance().setActivityService(null);
+	}
+	
+	public void setReferenceService(ReferenceService service) {
+		System.out.println("ReferenceService was set. Thank you DS!");
+		Services.getInstance().setReferenceService(service);
+	}
+	
+	public void unsetReferenceService(ReferenceService service) {
+		System.out.println("ReferenceService was unset. Why did you do this to me?");
+		Services.getInstance().setReferenceService(null);
+	}
+	
+	public void setRunService(RunService service) {
+		System.out.println("RunService was set. Thank you DS!");
+		Services.getInstance().setRunService(service);
+	}
+	
+	public void unsetRunService(RunService service) {
+		System.out.println("RunService was unset. Why did you do this to me?");
+		Services.getInstance().setRunService(null);
+	}
+	
+	public void setExecutionService(ExecutionService service) {
+		System.out.println("ExecutionService was set. Thank you DS!");
+		Services.getInstance().setExecutionService(service);
+	}
+	
+	public void unsetExecutionService(ExecutionService service) {
+		System.out.println("ExecutionService was unset. Why did you do this to me?");
+		Services.getInstance().setExecutionService(null);
 	}
 }
