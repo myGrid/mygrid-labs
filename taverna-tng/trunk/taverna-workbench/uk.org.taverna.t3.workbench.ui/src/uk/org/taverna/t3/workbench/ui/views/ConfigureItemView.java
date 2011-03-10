dcb02d3230c0b914567a16288ab4bf55d72d67b7
jits@mygrid.org.uk <jits@mygrid.org.uk@d2a11066-01d3-11df-8b0e-05a2803efa1e>
2011-03-10 17:42:36 +0000

    Shell for ConfigureItemViewer

--- taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/views/ConfigureItemView.java
+++ taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/views/ConfigureItemView.java
@@ -1,26 +1,49 @@
 package uk.org.taverna.t3.workbench.ui.views;
 
+import lombok.Getter;
+
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
+
+import uk.org.taverna.t3.workbench.ui.widgets.ConfigureItemViewer;
 
 public class ConfigureItemView extends ViewPart {
 	
 	public static final String ID = "uk.org.taverna.t3.workbench.ui.views.configureItem";	//$NON-NLS-1$
 
+	@Getter
+	private Composite parent;
+	
+	private ConfigureItemViewer configureItemViewer;
+	
 	public ConfigureItemView() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	@Override
 	public void createPartControl(Composite parent) {
-		// TODO Auto-generated method stub
-
+		this.parent = parent;
+		parent.setLayoutData(new GridData(GridData.FILL_BOTH));
+		GridLayout mainLayout = new GridLayout(1, true);
+		mainLayout.marginWidth = 0;
+		mainLayout.marginHeight = 0;
+		parent.setLayout(mainLayout);
+		
+		configureItemViewer = new ConfigureItemViewer(this, parent);
+		configureItemViewer.getControl().setLayoutData(new GridData(GridData.FILL_BOTH));
 	}
 
 	@Override
 	public void setFocus() {
-		// TODO Auto-generated method stub
-
+		configureItemViewer.setFocus();
+	}
+	
+	@Override
+	public void dispose() {
+		configureItemViewer.dispose();
+		super.dispose();
 	}
 
 }
