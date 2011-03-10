a8067f3e5daaa79d8ad4726a7a25b1b53cd96d25
Jiten Bhagat <mail@jits.co.uk>
2011-03-10 16:14:30 +0000

    Added the Console and ConfigureItem views into the UI (just as placeholders for now). Still needs some reworking to make this look better.

---
+++ taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/views/ConfigureItemView.java
@@ -0,0 +1,24 @@
+package uk.org.taverna.t3.workbench.ui.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class ConfigureItemView extends ViewPart {
+
+	public ConfigureItemView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
