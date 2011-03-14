87d0abb3abd51240a0af734b8326cdccf30a0c02
jits@mygrid.org.uk <jits@mygrid.org.uk@d2a11066-01d3-11df-8b0e-05a2803efa1e>
2011-03-14 14:29:25 +0000

    More refactorings for the Component Info Viewer

---
+++ taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/widgets/SectionFormTextWrapper.java
@@ -0,0 +1,60 @@
+package uk.org.taverna.t3.workbench.ui.widgets;
+
+import lombok.Getter;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.ui.forms.widgets.FormText;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.ScrolledForm;
+import org.eclipse.ui.forms.widgets.Section;
+import org.eclipse.ui.forms.widgets.TableWrapData;
+
+import com.google.common.base.Preconditions;
+
+public class SectionFormTextWrapper {
+	
+	private final FormToolkit toolkit;
+	private final ScrolledForm form;
+	
+	@Getter
+	private String title;
+	
+	private Section section;
+	private FormText formText;
+	
+	public SectionFormTextWrapper(FormToolkit toolkit, ScrolledForm form, String title) {
+		this.toolkit = toolkit;
+		this.form = form;
+		this.title = title;
+		this.createControls();
+	}
+
+	private void createControls() {
+		section = toolkit.createSection(form.getBody(), Section.TITLE_BAR | Section.TWISTIE);
+		formText = toolkit.createFormText(section, true);
+		section.setClient(formText);
+		TableWrapData td = new TableWrapData();
+		td.colspan = 1;
+		td.grabHorizontal = true;
+		section.setLayoutData(td);
+		section.setVisible(false);
+	}
+	
+	public void setTitle(String title) {
+		Preconditions.checkNotNull(title);
+		Preconditions.checkArgument(StringUtils.isNotBlank(title));
+		this.title = title;
+	}
+	
+	public void setContent(String content) {
+		setContent(content, false, true);
+	}
+	
+	public void setContent(String content, boolean parseTags, boolean expandURLs) {
+		section.setText(title);
+		formText.setText(content, parseTags, expandURLs);
+		section.setExpanded(true);
+		section.setVisible(true);
+	}
+	
+}
