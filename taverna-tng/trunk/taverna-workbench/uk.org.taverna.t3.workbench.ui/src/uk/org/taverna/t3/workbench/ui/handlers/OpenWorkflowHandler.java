0851e69bbbc48bbd283fb365a8063829cf1a0b0d
Jiten Bhagat <mail@jits.co.uk>
2011-03-16 10:47:55 +0000

    [TNG-153] Skeleton / placeholder for opening up existing workflows

---
+++ taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/handlers/OpenWorkflowHandler.java
@@ -0,0 +1,33 @@
+package uk.org.taverna.t3.workbench.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+
+public class OpenWorkflowHandler extends AbstractHandler implements IHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isHandled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+	}
+
+}
