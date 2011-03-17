d323bfc32cbd7e7d073ca57f7443a4ceaa4ee56c
David Withers <withers@users.noreply.github.com>
2011-03-17 17:33:28 +0000

    Improved workflow run status

--- taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/dialogs/WorkflowRunStatusDialog.java
+++ taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/dialogs/WorkflowRunStatusDialog.java
@@ -7,11 +7,14 @@ import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
 
-import uk.org.taverna.platform.report.ProcessorReport;
+import uk.org.taverna.t3.workbench.ui.models.ProcessorStatus;
 import uk.org.taverna.t3.workbench.ui.models.WorkflowRun;
 import uk.org.taverna.t3.workbench.ui.util.UIUtils;
 
@@ -28,27 +31,43 @@ public class WorkflowRunStatusDialog extends Dialog {
 	
 	@Override
 	protected void createButtonsForButtonBar(Composite parent) {
-		createButton(parent, IDialogConstants.CLOSE_ID,
+		createButton(parent, IDialogConstants.CANCEL_ID,
 	      IDialogConstants.CLOSE_LABEL, true);
 	}
 
 	@Override
 	protected Control createDialogArea(Composite parent) {
 		Composite container = (Composite) super.createDialogArea(parent);
-	
+
+		GridLayout mainLayout = new GridLayout(1, true);
+		mainLayout.marginWidth = 0;
+		mainLayout.marginHeight = 0;
+		container.setLayout(mainLayout);
+
 		TableViewer tableViewer = new TableViewer(container, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL
 				| SWT.FULL_SELECTION | SWT.BORDER);
+		Table table = tableViewer.getTable();
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
 		
 		UIUtils.createTableViewerColumn(tableViewer, "Processor", 150, 0);
-		UIUtils.createTableViewerColumn(tableViewer, "Status", 40, 1);
+		UIUtils.createTableViewerColumn(tableViewer, "Status", 80, 1);
 //		UIUtils.createTableViewerColumn(tableViewer, "Queued", 60, 2);
 //		UIUtils.createTableViewerColumn(tableViewer, "Complete", 60, 3);
 //		UIUtils.createTableViewerColumn(tableViewer, "Errors", 60, 4);
 		ViewerSupport.bind(
 				tableViewer,
-				new WritableList(workflowRun.getWorkflowReport().getAllProcessorReports(), ProcessorReport.class),
-				BeanProperties.values(ProcessorReport.class, new String[] { "processor.name", "state" }));
-		
+				new WritableList(workflowRun.getProcessors(), ProcessorStatus.class),
+				BeanProperties.values(ProcessorStatus.class, new String[] { "name", "state" }));
+
+		GridData gridData = new GridData();
+		gridData.verticalAlignment = GridData.FILL;
+		gridData.horizontalSpan = 1;
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.grabExcessVerticalSpace = true;
+		gridData.horizontalAlignment = GridData.FILL;
+		tableViewer.getControl().setLayoutData(gridData);
+
 		return container;
 	}
 }
--- taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/models/WorkflowRun.java
+++ taverna-tng/trunk/taverna-workbench/uk.org.taverna.t3.workbench.ui/src/uk/org/taverna/t3/workbench/ui/models/WorkflowRun.java
@@ -3,7 +3,11 @@ package uk.org.taverna.t3.workbench.ui.models;
 import java.beans.PropertyChangeListener;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
+import uk.org.taverna.platform.report.ProcessorReport;
 import uk.org.taverna.platform.report.WorkflowReport;
 
 public class WorkflowRun {
@@ -37,6 +41,14 @@ public class WorkflowRun {
 		return workflowReport;
 	}
 
+	public List<ProcessorStatus> getProcessors() {
+		List<ProcessorStatus> processors = new ArrayList<ProcessorStatus>();
+		Set<ProcessorReport> allProcessorReports = getWorkflowReport().getAllProcessorReports();
+		for (ProcessorReport processorReport : allProcessorReports) {
+			processors.add(new ProcessorStatus(processorReport));
+		}
+		return processors;
+	}
 
 	public void addPropertyChangeListener(String propertyName, PropertyChangeListener listener) {
 //		propertyChangeSupport.addPropertyChangeListener(propertyName, listener);
