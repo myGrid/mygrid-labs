001e9499661e462b5268c0b90fa666fb2435a548
Jiten Bhagat <mail@jits.co.uk>
2011-03-17 23:30:27 +0000

    Added search capabilities to the WCL client

--- taverna-tng/trunk/taverna-workbench/uk.org.taverna.wcl.api.client.tests/src/uk/org/taverna/wcl/api/client/tests/WclInstanceClientTest.java
+++ taverna-tng/trunk/taverna-workbench/uk.org.taverna.wcl.api.client.tests/src/uk/org/taverna/wcl/api/client/tests/WclInstanceClientTest.java
@@ -13,18 +13,27 @@ import junit.framework.TestCase;
 import org.junit.Test;
 
 import uk.org.taverna.t3.workbench.components.definitions.model.ComponentDefinition;
-import uk.org.taverna.wcl.api.client.WclException;
+import uk.org.taverna.wcl.api.client.WclApiClientException;
 import uk.org.taverna.wcl.api.client.WclInstanceClient;
 
 public class WclInstanceClientTest extends TestCase {
 	
 	@Test
-	public void testLoadingOfComponents() throws WclException, MalformedURLException {
+	public void testGetComponents() throws WclApiClientException, MalformedURLException {
 		WclInstanceClient client = new WclInstanceClient(new URL(WclClientTest.BASE_URL), WclClientTest.USER_AGENT);
 		
 		List<ComponentDefinition> defs = client.getComponents();
 		
 		assertThat(defs.size(), is(equalTo(20)));
+	}
+	
+	@Test
+	public void testGetComponentsSearch() throws WclApiClientException, MalformedURLException {
+		WclInstanceClient client = new WclInstanceClient(new URL(WclClientTest.BASE_URL), WclClientTest.USER_AGENT);
+		
+		List<ComponentDefinition> defs = client.getComponentsSearch("blast");
+		
+		assertTrue(defs.size() > 0);
 	}
 
 }
